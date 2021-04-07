c5edd92bebfedc75b0cef8eac553d5275032b6cf
Cogie <faridmcdato@gmail.com>
2021-04-07 23:41:03 +0800

    added code on home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!') }} testing for github
                 </div>
             </div>
         </div>
